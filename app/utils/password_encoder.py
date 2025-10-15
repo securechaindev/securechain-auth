ef0e7882d1e79b5841c99f5ec1129789d3c9acbf
GermanMT <germanoctako@gmail.com>
2025-10-15 18:10:26 +0200

    lint

--- app/utils/password_encoder.py
+++ app/utils/password_encoder.py
@@ -4,9 +4,9 @@ from passlib.context import CryptContext
 class PasswordEncoder:
     def __init__(self):
         self.context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-    
+
     async def hash(self, password: str) -> str:
         return self.context.hash(password)
-    
+
     async def verify(self, password: str, hashed_pass: str) -> bool:
         return self.context.verify(password, hashed_pass)
