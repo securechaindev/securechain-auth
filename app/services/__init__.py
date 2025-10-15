33336acb036ee640cfc408862bfd2c43b91e81fa
GermanMT <germanoctako@gmail.com>
2025-10-15 18:05:58 +0200

    feat: improve auth service class

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,15 +1,5 @@
-from .auth_service import (
-    create_revoked_token,
-    create_user,
-    is_token_revoked,
-    read_user_by_email,
-    update_user_password,
-)
+from .auth_service import AuthService
 
 __all__ = [
-    "create_revoked_token",
-    "create_user",
-    "is_token_revoked",
-    "read_user_by_email",
-    "update_user_password",
+    "AuthService",
 ]
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,47 +1,41 @@
 from datetime import datetime
 
-from app.logger import logger
 from app.models.auth import RevokedToken, User
 
 from .dbs.databases import get_graph_db_driver, get_odmantic_engine
 
 
-async def create_user(user: dict[str, str]) -> None:
-    engine = get_odmantic_engine()
-    query = """
-    create(u: User{
-        _id: $user_id
-    })
-    """
-    user: User = User(**user)
-    result = await engine.save(user)
-    user_id = str(result.id)
-    async with get_graph_db_driver().session() as session:
-        result = await session.run(query, user_id=user_id)
+class AuthService:
+    def __init__(self) -> None:
+        self._driver = get_graph_db_driver()
+        self._engine = get_odmantic_engine()
 
+    async def create_user(self, user: dict[str, str]) -> None:
+        query = """
+        create(u: User{
+            _id: $user_id
+        })
+        """
+        user: User = User(**user)
+        result = await self._engine.save(user)
+        user_id = str(result.id)
+        async with self._driver.session() as session:
+            result = await session.run(query, user_id=user_id)
 
-async def create_revoked_token(token: str, expires_at: datetime) -> None:
-    engine = get_odmantic_engine()
-    revoked_token = RevokedToken(token=token, expires_at=expires_at)
-    await engine.save(revoked_token)
+    async def create_revoked_token(self, token: str, expires_at: datetime) -> None:
+        revoked_token = RevokedToken(token=token, expires_at=expires_at)
+        await self._engine.save(revoked_token)
 
+    async def read_user_by_email(self, email: str) -> User:
+        user = await self._engine.find_one(User, User.email == email)
+        return user
 
-async def read_user_by_email(email: str) -> User:
-    engine = get_odmantic_engine()
-    user = await engine.find_one(User, User.email == email)
-    return user
+    async def update_user_password(self, user: User) -> None:
+        user_doc = await self._engine.find_one(User, User.email == user.email)
+        if user_doc:
+            user_doc.password = user.password
+            await self._engine.save(user_doc)
 
-
-async def update_user_password(user: User) -> None:
-    engine = get_odmantic_engine()
-    logger.info(user)
-    user_doc = await engine.find_one(User, User.email == user.email)
-    if user_doc:
-        user_doc.password = user.password
-        await engine.save(user_doc)
-
-
-async def is_token_revoked(token: str) -> bool:
-    engine = get_odmantic_engine()
-    revoked_token = await engine.find_one(RevokedToken, RevokedToken.token == token)
-    return revoked_token is not None
+    async def is_token_revoked(self, token: str) -> bool:
+        revoked_token = await self._engine.find_one(RevokedToken, RevokedToken.token == token)
+        return revoked_token is not None
