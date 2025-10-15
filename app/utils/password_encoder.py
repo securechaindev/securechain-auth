5b479a59920226abd03d0c4f18f960c5844c2724
GermanMT <germanoctako@gmail.com>
2025-10-15 18:05:40 +0200

    feat improce password encoder class

--- app/utils/password_encoder.py
+++ app/utils/password_encoder.py
@@ -1,10 +1,12 @@
 from passlib.context import CryptContext
 
-password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-async def get_hashed_password(password: str) -> str:
-    return password_context.hash(password)
-
-
-async def verify_password(password: str, hashed_pass: str) -> bool:
-    return password_context.verify(password, hashed_pass)
+class PasswordEncoder:
+    def __init__(self):
+        self.context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    
+    async def hash(self, password: str) -> str:
+        return self.context.hash(password)
+    
+    async def verify(self, password: str, hashed_pass: str) -> bool:
+        return self.context.verify(password, hashed_pass)
