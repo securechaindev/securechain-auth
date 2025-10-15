fc7d85805ab1f86713a2f5725f2a6ea7181cc3a9
GermanMT <germanoctako@gmail.com>
2025-10-15 17:57:47 +0200

    style: separate exceptions classes into different files

--- app/exceptions/__init__.py
+++ app/exceptions/__init__.py
@@ -1,11 +1,9 @@
-from .exceptions import (
-    ExpiredTokenException,
-    InvalidTokenException,
-    NotAuthenticatedException,
-)
+from .expired_token_exception import ExpiredTokenException
+from .invalid_token_exception import InvalidTokenException
+from .not_authenticated_exception import NotAuthenticatedException
 
 __all__ = [
     "ExpiredTokenException",
     "InvalidTokenException",
-    "NotAuthenticatedException"
+    "NotAuthenticatedException",
 ]
