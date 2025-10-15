8ebfe4bf43b78db3b9d3da62e9e5460dd74aac75
GermanMT <germanoctako@gmail.com>
2025-10-15 18:03:16 +0200

    feat: improve json encoder class

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,27 +1,9 @@
-from .json_encoder import json_encoder
-from .jwt_encoder import (
-    JWTBearer,
-    create_access_token,
-    create_refresh_token,
-    read_expiration_date,
-    set_auth_cookies,
-    verify_access_token,
-    verify_refresh_token,
-)
-from .password_encoder import (
-    get_hashed_password,
-    verify_password,
-)
+from .json_encoder import JSONEncoder
+from .jwt_encoder import JWTBearer
+from .password_encoder import PasswordEncoder
 
 __all__ = [
+    "JSONEncoder",
     "JWTBearer",
-    "create_access_token",
-    "create_refresh_token",
-    "get_hashed_password",
-    "json_encoder",
-    "read_expiration_date",
-    "set_auth_cookies",
-    "verify_access_token",
-    "verify_password",
-    "verify_refresh_token",
+    "PasswordEncoder",
 ]
